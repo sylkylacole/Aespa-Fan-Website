7c4873e29654ac159e3e7b45ac402d3c23bb0c4b
Syl'Kyla Cole <sylkylacole@gmail.com>
2025-01-28 12:48:32 -0600

    Started working on database and validation script

---
+++ member-quiz.js
@@ -0,0 +1,22 @@
+// Import questions, answers, etc etc and give an id or class
+
+const scoreArea = document.getElementById("score-div");
+
+
+
+// Set the score
+let score = 0;
+
+
+
+
+
+
+
+// Import the submit button
+
+const submit = document.getElementById("submit-button");
+
+// When you click submit, check if the answers are correct
+
+// Count correct answers and display it in a ratio via html below the title of the html page
