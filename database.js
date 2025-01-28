615b7caa8118d57910c415912d22f2fa02a84c23
Syl'Kyla Cole <sylkylacole@gmail.com>
2025-01-28 13:00:52 -0600

    Database basics

--- database.js
+++ database.js
@@ -1,5 +1,42 @@
 var data = [
     {
-        
-    }
+        question: "Who is this?",
+        answers: {
+            a: "Karina",
+            b: "Giselle",
+            c: "Winter",
+            d: "Ningning"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Who is this?",
+        answers: {
+            a: "Winter",
+            b: "Karina",
+            c: "Ningning",
+            d: "Giselle"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Who is this?",
+        answers: {
+            a: "Ningning",
+            b: "Giselle",
+            c: "Winter",
+            d: "Karina"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Who is this?",
+        answers: {
+            a: "Ningning",
+            b: "Giselle",
+            c: "Karina",
+            d: "Winter"
+        },
+        correctAnswer: "a"
+    },
 ]
--- member-quiz.js
+++ member-quiz.js
@@ -1,22 +1,42 @@
-// Import questions, answers, etc etc and give an id or class
+// // Import questions, answers, etc etc and give an id or class
 
-const scoreArea = document.getElementById("score-div");
-
-
-
-// Set the score
-let score = 0;
+// const scoreArea = document.getElementById("score-div");
 
 
+
+// // Set the score
+// let score = 0;
 
 
 
 
 
-// Import the submit button
 
-const submit = document.getElementById("submit-button");
 
-// When you click submit, check if the answers are correct
+// // Import the submit button
 
-// Count correct answers and display it in a ratio via html below the title of the html page
+// const submit = document.getElementById("submit-button");
+
+// // When you click submit, check if the answers are correct
+
+// // Count correct answers and display it in a ratio via html below the title of the html page
+
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+    function showQuestions(questions, quizContainer){
+        // code will go here
+    };
+
+    function showResults(questions, quizContainer, resultsContainer){
+        // code will go here
+    };
+
+    // show the questions
+    showQuestions(questions, quizContainer);
+
+    // when user clicks submit, show results
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    };
+};
