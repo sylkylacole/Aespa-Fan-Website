1d253ada9f31618c9a7db4b8d3dd8871e940a607
SylKyla <sylkylacole@gmail.com>
2025-08-19 16:17:22 -0500

    Got the quiz to add the score when selected. Need to make it to where the quiz does not continue to add on, but resets the score

--- member-quiz.js
+++ member-quiz.js
@@ -8,21 +8,21 @@ let score = 0;
 // create the function for what occurs when you click the button
 
 function submitQuiz() {
-    const q1 = document.getElementById("q1").value;
-    const q2 = document.getElementById("q2").value;
-    const q3 = document.getElementById("q3").value;
-    const q4 = document.getElementById("q4").value;
+    const q1 = document.getElementById("winter-radio.1");
+    const q2 = document.getElementById("giselle-radio.2");
+    const q3 = document.getElementById("karina-radio.3");
+    const q4 = document.getElementById("ningning-radio.4");
 
-    if (q1 === "c") {
+    if (q1.checked) {
         score += 1;
     }
-    if (q2 === "d") {
+    if (q2.checked) {
         score += 1;
     }
-    if (q3 === "d") {
+    if (q3.checked) {
         score += 1;
     }
-    if (q4 === "a") {
+    if (q4.checked) {
         score += 1;
     }
 
